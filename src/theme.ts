413dd51193b781469f516430829922e3d1bf103b
Matthew <matthewkimcs@gmail.com>
2023-04-03 13:52:15 -0700

    initial setup

---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/open-sans";
+
+const theme = extendTheme({
+  fonts: {
+    body: `'Open Sans', sans-serif`,
+  },
+});
+
+export default theme;
